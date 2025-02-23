5e10d581787e16f86433b03f6dc15b1697e7d449
PraveenKumarGarlapati <prvn012@gmail.com>
2025-02-23 20:48:11 +0530

    overall structure

---
+++ main.py
@@ -0,0 +1,45 @@
+#Import all packages
+
+import pandas as pd
+
+
+
+#Import necessary APIs, URLs and Keys
+
+
+
+#Import your Tool Accesses
+
+
+
+
+#Test cases to check all keys and tools accesses
+
+
+
+
+#Write down your workflow here
+
+
+
+#Query Translation Prompt
+
+
+
+
+#System Prompt
+
+
+
+
+#User Prompt
+
+
+
+
+#Save Final response to logger.csv
+
+
+
+
+
